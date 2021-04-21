2b904f625b344c016be1c061f9605ffa6f33dcf7
indu-229 <81751894+indu-229@users.noreply.github.com>
2021-04-21 13:25:13 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,7 +1,11 @@
-int exitcode = 0
+def test()
+{
+ int exitcode = 0
 for (slave in hudson.model.Hudson.instance.slaves) {
  if (slave.getComputer().isOffline().toString() == "true"){
  println('Slave ' + slave.name + " is offline!"); 
  exitcode++;
  }
 }
+}
+return this
